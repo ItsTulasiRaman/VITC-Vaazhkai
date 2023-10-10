fdab16058f29441bdc361ea582c5143a0d27e2bd
ItsTulasiRaman <tulasiraman917@gmail.com>
2023-10-10 23:16:32 +0530

    Project base setup

---
+++ components/Hero.js
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import top_left from "/public/images/ring-top-left.svg"
+
+export default function Hero(){
+    return(
+        <>
+        <section className="w-full h-[100vh] bg-black">
+            
+        </section>
+        </>
+    )
+} 
